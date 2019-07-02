962b15015b55c6a86afb43df79bc5c3fbee1ed15
Alexander Bazo <alexanderbazo@googlemail.com>
2019-07-02 17:34:14 +0200

    Add init sequence to solution

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 /* eslint-env node */
 
-const Config = require("./config.js"),
-  Logger = require("./lib/utils/Logger.js");
+const express = require("express"),
+  Config = require("./config.js"),
+  Logger = require("./lib/utils/Logger.js"),
+  ChatServer = require("./lib/ChatServer.js");
+
+var app = express();
 
 function init() {
-  Logger.log("Node.js application started", "Info");
+  startChatServer();
+  startClient();
+}
+
+function startChatServer() {
+  ChatServer.start(Config.WS_PORT);
+}
+
+function startClient() {
+  app.use(Config.CLIENT_ROUTE, express.static(Config.CLIENT_PATH));
+  app.listen(Config.CLIENT_PORT, function() {
+    Logger.log(
+      `Client served at http://localhost:${Config.CLIENT_PORT}${Config.CLIENT_ROUTE}`,
+      "HTTP"
+    );
+  });
 }
 
 init();
